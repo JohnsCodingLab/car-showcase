86379fac1fe00ef4d8eeae06ebf455e02c68409f
Levi John Favour <engrlevijohn@gmail.com>
2023-09-16 19:13:21 +0100

    First commit through VS Code

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata: Metadata = {
+  title: 'Car Rental Hub',
+  description: 'Come and Discover the best cars in the world',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <Navbar />
+      <body className='relative'>{children}</body>
+      <Footer />
+    </html>
+  );
+}
---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react';
+import Image from '@/node_modules/next/image';
+import { CustomButtonProps } from '@/types/index';
+
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || 'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
